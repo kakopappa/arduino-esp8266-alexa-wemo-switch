d618596b9c400e9af725df8bdd7c5c73369082d4
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2017-12-19 10:29:04 +0700

    'M-SEARCH' issue fixed.
    'M-SEARCH' changed to "M-SEARCH"
    
    indexOf('M-SEARCH') only looks for 'M' and not for 'M-SEARCH'. Use "M-SEARCH" instead (with double quotes).
    
    https://github.com/witnessmenow/esp8266-alexa-wemo-emulator/issues/7

--- sinric.ino
+++ sinric.ino
@@ -92,7 +92,7 @@ void loop() {
         //Serial.println("Request:");
         //Serial.println(request);
          
-        if(request.indexOf('M-SEARCH') > 0) {
+        if(request.indexOf("M-SEARCH") > 0) {
             // Issue https://github.com/kakopappa/arduino-esp8266-alexa-multiple-wemo-switch/issues/22 fix
             //if(request.indexOf("urn:Belkin:device:**") > 0) {
              if((request.indexOf("urn:Belkin:device:**") > 0) || (request.indexOf("ssdp:all") > 0) || (request.indexOf("upnp:rootdevice") > 0)) {
